80b7e83a2010d17b9e3fa03f3401d0dc5d6e8544
Esther Fummey <e.fummey@appsNmobile.local>
2025-12-09 07:23:21 +0000

    first commit

---
+++ firstcode.js
@@ -0,0 +1,158 @@
+
+// yesuko tutorial
+// var humanTemp = 30
+// const humanName = "Kofi"
+// var humanAge = 77
+// humanTemp = [20, 40]
+// var humanName2 = humanName
+// humanName2 = "Abena"
+// var humanInfo = {
+//     "name": "kofi",
+//     "age" : 2
+// }
+// console.log(humanTemp)
+// console.log(humanName)
+// console.log(humanName2)
+// console.log(humanInfo)
+
+// data types)(base or primitive)
+// string = "kofi", "Ama ", "he is going to work", "52", "52Slack"
+// Numbers = 1, 3, 10 ,100, 2.2, 10000.5
+// big integers 100000, 8 , 90000000000(very huge numbers and vey huge decimal numbers)
+// boolean = true or false
+
+// composite or derive data types
+// array - stores more than one data type [1, "m", 2]
+// object - stor {1: 4, "name": "ama", "age": 77}
+
+
+
+
+
+
+
+
+
+
+// var number = 5 // in-line comment
+// /* mltiple comment gnf  */
+// /* 
+// Date Types; undefined, null, booolean, string, symbol, number, and object
+// */
+// var myName = "Beau"
+//  myName = 8
+
+
+// let ourName = "freeCodeCamp"
+// const pi = 12.2
+// /* var can be used everywhere and let can be used within a scope */
+// var a  // to declare
+// var b= 2 // to assign
+
+// console.log(a) // show in the console
+
+// // initializing a variable and declaring
+// var a = 9 // var a means initialise and assigning
+
+// // we use camelCase in Js ex var styB = 9
+
+// // we have addtion , subtracting, and multiplication
+// var b = 1 + 2
+// var c = 1-2
+// var g = 10/5
+// // increament
+// b++
+// //decreamemt
+// b--
+
+// // decial var myDecail = 0.11
+
+// // 
+// //reminder sign
+// // var reminder
+// reminder = 22 % 5
+
+// // a+= 12, b+= 12 instead b = b+ 12 etc
+
+// // for quotation or escape characters
+// //chat please give me all the escape characters
+// var myQuote = " I said \"I am Happy\""
+// console.log(myQuote)
+
+
+//conatenationg
+// var ourName = "freecodecamp"
+// let ourStr = "Hello " + ourName + " googd"
+// console.log(ourStr)
+
+//finding length
+// var a = ourName.length
+// console.log(a)
+
+//finding index 
+
+//changing to string or the characters
+//ourName[1] = "h"
+//console.log(ourName) // gives an error
+
+// var b = ourName.length - 1
+// console.log(b
+// +, * , / , %
+//type conversion
+// var b = ""
+// var c = 20
+// var r  =  String(b) 
+// var r1 = Boolean(b)
+//console.log(r)
+
+//operaters
+
+//modulos
+// var num = 11
+
+// console.log(num % 2 == 0)
+//console.log(2 != "2")
+// var age = readline("Enter age: ")
+// if (age >=  18) {
+//     console.log("Allow person to enter moview theatre")
+    
+// } else if (age >= 40){
+//     console.log("this is a parent , dont allow entry")
+
+// }
+// else {
+//     console.log("under age, go home and sleep!")
+// }
+
+//Loops
+// var condition = true
+// var counter = 0
+// while(condition){
+//     console.log("Running Js")
+//     counter ++
+//     console.log('current counter value: ', counter)
+//     if(counter == 50){
+//         console.log('counters value: %d', counter)
+//         condition = false
+//     }
+    
+// }
+
+// var condition = true
+// var counter = 0
+// do {
+//     console.log("Running Js")
+//     counter ++
+//     console.log('current counter value: ', counter)
+//     if(counter == 50){
+//         console.log('counters value: %d', counter)
+//         condition = false
+//     }
+
+// }while(condition)
+
+// do{
+//   console.log(1+1)
+// }while(true)
+var b = ['Ama', 2, true]
+console.log(b[0]) 
